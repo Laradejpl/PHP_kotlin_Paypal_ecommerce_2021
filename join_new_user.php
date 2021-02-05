cf41e9b4c96d601ef79db73dde9f170bdcd6aeb2
laradejpl <51596185+Laradejpl@users.noreply.github.com>
2021-02-05 21:14:02 +0100

    ajout du fichier fetch_brands

--- join_new_user.php
+++ join_new_user.php
@@ -7,7 +7,7 @@ $connection = new mysqli("185.98.131.128","regga1388946","v56m4wtkss","regga1388
 
 
 
-$emailCheckSqlcommand = $connection->prepare("SELECT * FROM  app_users_table WHERE email = ?");
+$emailCheckSqlcommand = $connection->prepare("SELECT * FROM  user_kotlin_app WHERE email = ?");
 
 $emailCheckSqlcommand->bind_param("s",$_GET['email']);
 
@@ -15,22 +15,42 @@ $emailCheckSqlcommand->execute();
 
 $emailResult = $emailCheckSqlcommand->get_result();
 
+
+if(!empty($_GET['email'])){
+
+    //echo'Enter your email';
+
 if ($emailResult->num_rows == 0) {
 
 
-    $sqlcommand = $connection->prepare("insert into app_users_table values (?,?,?)");
+    $sqlcommand = $connection->prepare("insert into user_kotlin_app values (?,?,?,?)");
 
-    $sqlcommand->bind_param("sss",$_GET['email'],$_GET['username'],$_GET['pass']);
+    $sqlcommand->bind_param("isss",$_GET['id'],$_GET['email'],$_GET['username'],$_GET['pass']);
 
     $sqlcommand->execute();
 
-    echo 'vous avez été enregistrez. ';
+    echo 'Congratulation! The registration process was successful ';
+    
+    unset($_GET);
+
+}
+}
 
 
-}else {
+
+
+
+else {
     
     echo 'a user with the same address already exists ';
 }
+
+
+
+
+
+
+
 
 
 
--- login_app_user.php
+++ login_app_user.php
@@ -1,7 +1,7 @@
 <?php
 $dc = new mysqli("185.98.131.128","regga1388946","v56m4wtkss","regga1388946");
 
-$check_login_info = $dc->prepare("SELECT * FROM app_users_table WHERE email = ? AND pass = ?"); 
+$check_login_info = $dc->prepare("SELECT * FROM user_kotlin_app WHERE email = ? AND pass = ?"); 
 
 $check_login_info->bind_param("ss",$_GET['email'],$_GET['pass']);
 
@@ -10,23 +10,52 @@ $check_login_info->execute();
 $login_result = $check_login_info->get_result();
 
 
+   
 
-if ($login_result->num_rows == 0) {
-
-
-   // $sqlcommand = $connection->prepare("insert into app_users_table values (?,?,?)");
-
-   // $sqlcommand->bind_param("sss",$_GET['email'],$_GET['username'],$_GET['pass']);
-
-   // $sqlcommand->execute();
-
-    echo 'vous avez été enregistrez. ';
+   
 
 
-}else {
+if ($login_result->num_rows == 0 ) {
+
+
+   
+
+    echo 'The user does not exist ';
+
     
-    echo 'a user with the same address already exists ';
+
+
+}elseif(empty($_GET['email'])){
+
+    echo'Enter your email';
+
+
+
+
+
 }
+
+elseif(empty($_GET['pass'])){
+
+    echo'Enter your password';
+
+
+
+
+
+}
+
+
+
+else {
+    
+    echo 'The user does exist ';
+    unset($_GET);
+}
+
+
+
+
 
 
 
